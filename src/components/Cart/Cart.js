194a64bcf16ed5272e67881ac2d1b07be9a7e912
rudkevych <oksana.rudkevych@gmail.com>
2022-10-30 00:36:28 +0200

    fix: show cart items from state

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,27 @@
+import { useSelector } from 'react-redux';
 import Card from '../UI/Card';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
 
 const Cart = (props) => {
+  const items = useSelector((state) => state.cart.items);
+  //TODO: pass item
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
+        {items.map((item) => (
+          <CartItem
+            item={{
+              title: item.title,
+              quantity: item.quantity,
+              total: item.total,
+              price: item.price,
+              id: item.id,
+            }}
+            key={item.id}
+          />
+        ))}
       </ul>
     </Card>
   );
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,13 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialCartState = {
-  items: [],
-  totalQuantity: 100
+  items: [{
+    title: 'hello',
+    description: 'helloo',
+    price: 10,
+    id: 1,
+    quantity: 3,
+    total: 30
+  }],
+  totalQuantity: 0
 };
 const cartSlice = createSlice({
   name: 'cart',
   initialState: initialCartState,
-  reducers: {},
+  reducers: {
+    addItem(state, action) {
+      const itemToAdd = action.payload;
+
+      state.totalQuantity++;
+    },
+    removeItem(state, action) {
+      const id = action.payload.id;
+    }
+  },
 });
 
 export const cartReducer = cartSlice.reducer;
