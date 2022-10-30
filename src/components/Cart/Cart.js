c174016efca496648a4ad63496f74517fa544a18
rudkevych <oksana.rudkevych@gmail.com>
2022-10-30 14:21:14 +0100

    fix: cart items error added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,18 +5,19 @@ import CartItem from './CartItem';
 
 const Cart = (props) => {
   const items = useSelector((state) => state.cart.items);
+
   console.log(items);
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
-      <ul>
-        {items.map((item) => (
-          <CartItem
-            item={item}
-            key={item.id}
-          />
-        ))}
-      </ul>
+      {!items.length && <p>Cart is empty :(</p>}
+      {items.length > 0 && (
+        <ul>
+          {items.map((item) => (
+            <CartItem item={item} key={item.id} />
+          ))}
+        </ul>
+      )}
     </Card>
   );
 };
