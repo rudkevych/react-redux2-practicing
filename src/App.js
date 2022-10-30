60bc0693d655e08b8772e2438a4bb32ffd67413f
rudkevych <oksana.rudkevych@gmail.com>
2022-10-30 15:42:21 +0100

    feat: http request added for cart updating

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
   const cartIsVisible = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(
+      'https://react-redux2-practicing-default-rtdb.firebaseio.com/cart.json',
+      {
+        method: 'PUT',
+        body: JSON.stringify(cart),
+      }
+    );
+  }, [cart]);
 
   return (
     <Layout>
